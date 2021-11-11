4b996d2bdb4192689e3dab4bebdf69373c87bc20
ji-park <qkrskdjf@naver.com>
2021-11-12 03:32:39 +0900

    입금 api

--- src/deposit/deposit.controller.ts
+++ src/deposit/deposit.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get } from '@nestjs/common';
+import { UpdateDepositDto } from './dto/update.dto'
+import { DepositService } from './deposit.service';
 @Controller('deposit')
-export class DepositController {}
+export class DepositController {
+    constructor(private depositService: DepositService) { }
+
+    @Get()
+    deposite(@Body() updateDepositDto: UpdateDepositDto) {
+        //jwt 해석해서 userid 넘겨줘야함
+        return this.depositService.deposit(UpdateDepositDto, User);
+    }
+}
--- src/deposit/deposit.service.ts
+++ src/deposit/deposit.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, } from '@nestjs/common';
+import { Connection, Repository } from "typeorm";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Balance } from "../balance/balance.entity";
+import { Deposit } from "../deposit/deposit.entity";
+import { Account } from "../account/account.entity";
+import { User } from "../user/user.entity";
 
-@Injectable()
-export class DepositService {}
+export class DepositService {
+
+
+    constructor(
+        @InjectRepository(Deposit) private depositRepository: Repository<Deposit>,
+        @InjectRepository(Balance) private balanceRepository: Repository<Balance>,
+        @InjectRepository(Account) private accountRepository: Repository<Account>,
+        private connection: Connection,
+    ) { }
+
+    async findByAccountNumber(accountNumber: String): Promise<any> {
+        return await this.accountRepository.findOne({
+            where: { accountNumber }
+        })
+    }
+
+    async authCheck(accountId: Number, userId: Number): Promise<any> {
+        return await this.accountRepository
+            .findOne({
+                where: { id: accountId, user: { id: userId } },
+                relations: ['user']
+            });
+    }
+
+    async update(updateQuestionInfo, user) {
+        const { depositAmount, accountNumber } = updateQuestionInfo;
+
+        // 해당 계좌 정보 조회
+        const account = await this.findByAccountNumber(accountNumber);
+        if (account === undefined) {
+            throw new Error("The account doesn't exist.");
+        }
+        // 권한 조회
+        const auth = await this.authCheck(account.id, user);
+        if (auth === undefined) {
+            throw new Error("You don't have edit permission");
+        }
+        const queryRunner = this.connection.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            // 잔액 조회
+            const nowBalance = await queryRunner.manager
+                .getRepository(Balance)
+                .findOne({ where: { account } });
+            // 입금 내역 생성
+            const newBalance = nowBalance.balance + depositAmount
+            const deposit = await queryRunner.manager
+                .getRepository(Deposit)
+                .save({ accountId: account.id, oldBalance: nowBalance.balance, newBalance: newBalance, amount: depositAmount })
+            await queryRunner.commitTransaction();
+        } catch (error) {
+            console.error(error);
+            await queryRunner.rollbackTransaction();
+            throw error;
+        } finally {
+            await queryRunner.release();
+        }
+    }
+}
