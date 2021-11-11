2f35eccf3037b0939931d1f27c9d326b028b988f
YeomJaeSeon <a89541457@gmail.com>
2021-11-12 01:31:36 +0900

    pr test

--- src/balance/balance.module.ts
+++ src/balance/balance.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BalanceService } from './balance.service';
-import { BalanceController } from './balance.controller';
-
+import { BalanceController } from "./balance.controller";
+//test - prettier
 @Module({
   controllers: [BalanceController],
   providers: [BalanceService],
