9786d5e4dba3f1486465989ed033323c0438ef7f
taehee <wonderfulhuman@naver.com>
2021-11-13 04:59:29 +0900

    Feat: Resolver 반환 타입 수정

--- src/account/account.resolver.ts
+++ src/account/account.resolver.ts
@@ -1,13 +1,20 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { TransactionHistory } from './graph.transaction.history.entity';
 import { AccountQueryService } from './account.query.service';
+import { TransactionHistoryPaginationRequest } from './dto/transaction.history.pagination.request';
+import { ValidationPipe } from '@nestjs/common';
 
 @Resolver()
 export class AccountResolver {
   constructor(private accountQueryService: AccountQueryService) {}
 
   @Query(() => [TransactionHistory])
-  async getTransactionHistory(): Promise<TransactionHistory[]> {
-    return await this.accountQueryService.getTransactionHistory();
+  async getTransactionHistory(
+    @Args(new ValidationPipe({ transform: true }))
+    transactionHistoryPaginationRequest: TransactionHistoryPaginationRequest,
+  ): Promise<[TransactionHistory[], number]> {
+    return this.accountQueryService.getTransactionHistory(
+      transactionHistoryPaginationRequest,
+    );
   }
 }
