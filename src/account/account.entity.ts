8d0b27ceb55c8d1e08cad85ab124b53f30a4559b
taehee <wonderfulhuman@naver.com>
2021-11-12 17:26:12 +0900

    Feat: Account의 Deposit과 Withdraw 잘못된 매핑 수정

--- src/account/account.entity.ts
+++ src/account/account.entity.ts
@@ -1,9 +1,10 @@
-import { Entity, ManyToOne, OneToMany, OneToOne, JoinColumn } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, OneToMany, OneToOne } from 'typeorm';
 import { Base } from '../base.entity/base.entity';
 import { User } from '../user/user.entity';
 import { Deposit } from 'src/deposit/deposit.entity';
 import { Withdraw } from 'src/withdraw/withdraw.entity';
 import { Balance } from 'src/balance/balance.entity';
+
 @Entity()
 export class Account extends Base {
   userId: number;
@@ -14,10 +15,10 @@ export class Account extends Base {
 
   accountNumber: string;
 
-  @OneToMany(() => Deposit, (deposit) => deposit.accountId)
+  @OneToMany(() => Deposit, (deposit) => deposit.account)
   deposits: Deposit[];
 
-  @OneToMany(() => Withdraw, (withdraw) => withdraw.accountId)
+  @OneToMany(() => Withdraw, (withdraw) => withdraw.account)
   withdraws: Withdraw[];
 
   @ManyToOne(() => User, (user) => user.accounts, { onDelete: 'CASCADE' })
