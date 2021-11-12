53dcf08a8251941ffe1f51331c613ff496472470
taehee <wonderfulhuman@naver.com>
2021-11-12 17:39:40 +0900

    Feat: WithDraw Account테이블과 조인하여 커버링 인덱스 생성

--- src/withdraw/withdraw.query.repository.ts
+++ src/withdraw/withdraw.query.repository.ts
@@ -4,9 +4,8 @@ import { Withdraw } from './withdraw.entity';
 @EntityRepository(Withdraw)
 export class WithdrawQueryRepository extends Repository<Withdraw> {
   getTransactionHistory(): Promise<Withdraw[]> {
-    return this.createQueryBuilder('boards')
-      .innerJoinAndSelect('boards.user', 'user')
-      .select(['boards', 'user.userId', 'user.nickname'])
+    return this.createQueryBuilder('withdraw')
+      .innerJoinAndSelect('withdraw.account', 'account')
       .getMany();
   }
 }
