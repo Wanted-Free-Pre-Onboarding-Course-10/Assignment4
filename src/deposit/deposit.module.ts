d00bb4cf13cc89849d77d72922258a184f7b3d4f
taehee <wonderfulhuman@naver.com>
2021-11-12 17:36:58 +0900

    Feat: DepositQueryRepository사용을 위한 TypeOrmModule Import

--- src/deposit/deposit.module.ts
+++ src/deposit/deposit.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DepositController } from './deposit.controller';
 import { DepositService } from './deposit.service';
+import { DepositQueryService } from './deposit.query.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepositQueryRepository } from './deposit.query.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DepositQueryRepository])],
   controllers: [DepositController],
-  providers: [DepositService],
+  providers: [DepositService, DepositQueryService],
 })
 export class DepositModule {}
