af02b893653c112348c5734c034bd624deb39e9c
taehee <wonderfulhuman@naver.com>
2021-11-12 15:18:04 +0900

    Feat: Account Resolver 생성

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { AccountResolver } from './account.resolver';
 
 @Module({
   controllers: [AccountController],
-  providers: [AccountService],
+  providers: [AccountService, AccountResolver],
 })
 export class AccountModule {}
