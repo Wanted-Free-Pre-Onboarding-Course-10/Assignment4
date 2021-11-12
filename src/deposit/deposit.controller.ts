58b0fbe10a57b8ab021457b0383c3bbec1402edc
ji-park <qkrskdjf@naver.com>
2021-11-12 17:11:27 +0900

    Feat : 잔액 조회 기능 수정

--- src/deposit/deposit.controller.ts
+++ src/deposit/deposit.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UpdateDepositDto } from './dto/update.dto'
 import { DepositService } from './deposit.service';
 @Controller('deposit')
 export class DepositController {
     constructor(private depositService: DepositService) { }
 
-    @Get()
+    @Post('/')
     deposite(@Body() updateDepositDto: UpdateDepositDto) {
         //jwt 해석해서 userid 넘겨줘야함
-        return this.depositService.deposit(UpdateDepositDto, User);
+        return this.depositService.deposit(updateDepositDto, 1);
     }
 }
--- src/deposit/deposit.module.ts
+++ src/deposit/deposit.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DepositController } from './deposit.controller';
 import { DepositService } from './deposit.service';
-
+import { Balance } from "../balance/balance.entity";
+import { Deposit } from "../deposit/deposit.entity";
+import { Account } from "../account/account.entity";
+import { User } from "../user/user.entity";
 @Module({
+  imports: [TypeOrmModule.forFeature([Balance, Deposit, Account, User])],
   controllers: [DepositController],
   providers: [DepositService],
 })
-export class DepositModule {}
+export class DepositModule { }
