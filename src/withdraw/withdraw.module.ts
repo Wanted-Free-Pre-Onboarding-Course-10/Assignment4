066853a50221537283e2c38d5acbd9910394695e
taehee <wonderfulhuman@naver.com>
2021-11-13 05:06:21 +0900

    Feat: WithdrawQueryRepository 추가

--- src/withdraw/withdraw.module.ts
+++ src/withdraw/withdraw.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WithdrawService } from './withdraw.service';
 import { WithdrawController } from './withdraw.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WithdrawQueryRepository } from './withdraw.query.repository';
+import { WithdrawQueryService } from './withdraw.query.service';
 
 @Module({
-  providers: [WithdrawService],
+  imports: [TypeOrmModule.forFeature([WithdrawQueryRepository])],
+  providers: [WithdrawService, WithdrawQueryService],
   controllers: [WithdrawController],
 })
 export class WithdrawModule {}
