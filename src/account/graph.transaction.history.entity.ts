76de6c42c1e7fb5b00998ba019a70414c26582e7
taehee <wonderfulhuman@naver.com>
2021-11-12 17:32:45 +0900

    Feat: TransactionHistory GraphQL Docs작성
    - 입금과 출금의 테이블이 나뉘어져있으므로 TransactionHistory로 통일해서 반환해야한다.

--- src/account/graph.transaction.history.entity.ts
+++ src/account/graph.transaction.history.entity.ts
@@ -1,12 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Deposit } from '../deposit/deposit.entity';
+import { Withdraw } from '../withdraw/withdraw.entity';
 
 @ObjectType()
 export class TransactionHistory {
-  @Field()
-  id!: string;
+  constructor(
+    id: number,
+    transactionDate: Date,
+    transactionAmount: number,
+    balance: number,
+    transactionType: string,
+    etc: string,
+  ) {
+    this.id = id;
+    this.transactionDate = transactionDate;
+    this.transactionAmount = transactionAmount;
+    this.balance = balance;
+    this.transactionType = transactionType;
+    this.etc = etc;
+  }
 
   @Field()
-  transactionDate: string;
+  id!: number;
+
+  @Field()
+  transactionDate: Date;
 
   @Field()
   transactionAmount: number;
@@ -19,4 +37,26 @@ export class TransactionHistory {
 
   @Field()
   etc: string;
+
+  static ofDeposit(deposit: Deposit): TransactionHistory {
+    return new TransactionHistory(
+      deposit.id,
+      deposit.created_at,
+      deposit.amount,
+      deposit.newBalance,
+      '입금',
+      '비고',
+    );
+  }
+
+  static ofWithdraw(withdraw: Withdraw): TransactionHistory {
+    return new TransactionHistory(
+      withdraw.id,
+      withdraw.created_at,
+      withdraw.amount,
+      withdraw.newBalance,
+      '출금',
+      '비고',
+    );
+  }
 }
