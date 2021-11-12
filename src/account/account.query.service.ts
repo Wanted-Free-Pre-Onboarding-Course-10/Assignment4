b93b13e2456119e21f5bbf329cab8709d1c8b656
taehee <wonderfulhuman@naver.com>
2021-11-13 04:57:45 +0900

    Feat: Search Type에 따라 로직분리

--- src/account/account.query.service.ts
+++ src/account/account.query.service.ts
@@ -2,20 +2,83 @@ import { Injectable } from '@nestjs/common';
 import { DepositQueryService } from '../deposit/deposit.query.service';
 import { WithdrawQueryService } from '../withdraw/withdraw.query.service';
 import { TransactionHistory } from './graph.transaction.history.entity';
+import { TransactionHistoryPaginationRequest } from './dto/transaction.history.pagination.request';
+import { AccountQueryRepository } from './account.query.repository';
 
 @Injectable()
 export class AccountQueryService {
   constructor(
     private depositQueryService: DepositQueryService,
     private withdrawQueryService: WithdrawQueryService,
+    private accountQueryRepository: AccountQueryRepository,
   ) {}
 
-  async getTransactionHistory() {
-    const deposits = await this.depositQueryService.getTransactionHistory();
-    const withdraws = await this.withdrawQueryService.getTransactionHistory();
-    return [
-      ...deposits.map((deposit) => TransactionHistory.ofDeposit(deposit)),
-      ...withdraws.map((withdraw) => TransactionHistory.ofWithdraw(withdraw)),
-    ];
+  async getTransactionHistory(
+    transactionHistoryPaginationRequest: TransactionHistoryPaginationRequest,
+  ): Promise<[TransactionHistory[], number]> {
+    const accountId: number = await this.accountQueryRepository.getAccountId(
+      transactionHistoryPaginationRequest,
+    );
+
+    const depositsPaginationRequest =
+      this.depositQueryService.getTransactionHistory(
+        transactionHistoryPaginationRequest,
+        accountId,
+      );
+
+    const withdrawPaginationRequest =
+      this.withdrawQueryService.getTransactionHistory(
+        transactionHistoryPaginationRequest,
+        accountId,
+      );
+
+    const searchType = transactionHistoryPaginationRequest.searchType;
+
+    console.log(searchType);
+    if (searchType === 'ALL') {
+      const [deposits, depositCount] =
+        await this.depositQueryService.getTransactionHistory(
+          transactionHistoryPaginationRequest,
+          accountId,
+        );
+      const [withdraws, withdrawCount] =
+        await this.withdrawQueryService.getTransactionHistory(
+          transactionHistoryPaginationRequest,
+          accountId,
+        );
+      return [
+        [
+          ...deposits.map((deposit) => TransactionHistory.ofDeposit(deposit)),
+          ...withdraws.map((withdraw) =>
+            TransactionHistory.ofWithdraw(withdraw),
+          ),
+        ],
+        depositCount + withdrawCount,
+      ];
+    } else if (searchType === 'DEPOSIT') {
+      const [deposits, depositCount] =
+        await this.depositQueryService.getTransactionHistory(
+          transactionHistoryPaginationRequest,
+          accountId,
+        );
+      return [
+        [...deposits.map((deposit) => TransactionHistory.ofDeposit(deposit))],
+        depositCount,
+      ];
+    } else if (searchType === 'WITHDRAW') {
+      const [withdraws, withdrawCount] =
+        await this.withdrawQueryService.getTransactionHistory(
+          transactionHistoryPaginationRequest,
+          accountId,
+        );
+      return [
+        [
+          ...withdraws.map((withdraw) =>
+            TransactionHistory.ofWithdraw(withdraw),
+          ),
+        ],
+        withdrawCount,
+      ];
+    }
   }
 }
