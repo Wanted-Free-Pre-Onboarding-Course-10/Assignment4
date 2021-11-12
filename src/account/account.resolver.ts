07d32af650a9439779b896b153051a6ee592a080
taehee <wonderfulhuman@naver.com>
2021-11-12 17:31:45 +0900

    Feat: 검색 API를 위한 AccountResolver에 Query추가

--- src/account/account.resolver.ts
+++ src/account/account.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { TransactionHistory } from './graph.transaction.history.entity';
+import { AccountQueryService } from './account.query.service';
 
 @Resolver()
-export class AccountResolver {}
+export class AccountResolver {
+  constructor(private accountQueryService: AccountQueryService) {}
+
+  @Query(() => [TransactionHistory])
+  async getTransactionHistory(): Promise<TransactionHistory[]> {
+    return await this.accountQueryService.getTransactionHistory();
+  }
+}
