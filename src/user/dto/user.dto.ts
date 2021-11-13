3125bdaf6400f79efe1d8c6271f090ccf3862bb6
ji-park <qkrskdjf@naver.com>
2021-11-13 21:08:47 +0900

    hotfix : USERDTO 삭제

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,16 +1,9 @@
 import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
 
-export class UserDto{
+export class UserDto {
   @IsNotEmpty()
-  @IsString()
-  @Length(5, 50)
   username: string;
 
   @IsNotEmpty()
-  @IsString()
-  @Length(5, 50)
-  @Matches(/^[a-zA-Z0-9]*$/,{
-    message: 'password only accepts eng and num'
-  })
   password: string;
 }
